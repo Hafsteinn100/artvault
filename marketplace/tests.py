db0a3a4ff311db0f4c1a2aaeca7b11f7747f41af
Hafsteinn100 <hafsteinnhugi1@gmail.com>
2026-05-06 20:35:37 +0000

    Build ArtVault homepage

--- marketplace/tests.py
+++ marketplace/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+    def test_homepage_renders_artvault_homepage(self):
+        response = self.client.get(reverse('home'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Buy or sell art')
+        self.assertContains(response, 'Latest Pieces')
+        self.assertContains(response, 'Amber Stillness')
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    latest_pieces = [
+        {
+            'name': 'Amber Stillness',
+            'medium': 'Oil',
+            'price': '$100',
+            'image': 'marketplace/images/piece-amber.svg',
+        },
+        {
+            'name': 'Blue Hour Study',
+            'medium': 'Watercolor',
+            'price': '$100',
+            'image': 'marketplace/images/piece-blue.svg',
+        },
+        {
+            'name': 'Quiet Geometry',
+            'medium': 'Acrylic',
+            'price': '$100',
+            'image': 'marketplace/images/piece-geometry.svg',
+        },
+    ]
+
+    return render(request, 'marketplace/index.html', {'latest_pieces': latest_pieces})
