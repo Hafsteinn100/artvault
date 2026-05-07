7348e278f4933637b28ac8a135e796108420da78
Hafsteinn100 <hafsteinnhugi1@gmail.com>
2026-05-07 11:32:58 +0000

    Homepage base

---
+++ index.js
@@ -0,0 +1,8 @@
+const header = document.querySelector(".site-header");
+
+function updateHeaderGlass() {
+  header.classList.toggle("is-scrolled", window.scrollY > 12);
+}
+
+updateHeaderGlass();
+window.addEventListener("scroll", updateHeaderGlass, { passive: true });
