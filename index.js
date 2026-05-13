515d989211bcbbb4787711907dd02c94c8350768
Atlihero <atlihero@gmail.com>
2026-05-13 16:23:59 +0000

    oppa

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ window.addEventListener("scroll", updateHeaderGlass, { passive: true });
 
 document.addEventListener("DOMContentLoaded", function () {
     const img = document.getElementById("homepage-profile");
+    const name = document.getElementById("homepage-username"); // optional if you display it
 
     const savedImage = localStorage.getItem("profileImage");
+    const savedName = localStorage.getItem("profileName");
 
     img.src = savedImage || "profile.png";
+
+    if (name && savedName) {
+        name.textContent = savedName;
+    }
 });
